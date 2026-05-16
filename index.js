836c9e1f983f1b5288f63a40cda12aa0c35b2501
mohammed ED-DAHBY <mincharyt@gmail.com>
2026-05-16 16:38:38 +0100

    version java

--- index.js
+++ index.js
@@ -9,11 +9,28 @@ console.log(email);
 console.log(password);
 console.log(button);
 
+const savedusername = JSON.parse(localStorage.getItem("username") || "null");
+if (savedusername) {
+    nom.value = savedusername || "";
+    email.value = savedusername || "";
+    password.value = savedusername || "";
+}
+
+button.addEventListener("click", function(event){
+    event.preventDefault();
+
+    const user ={
+        username: nom.value,
+        email: email.value,
+        password: password.value
+    };
+    localStorage.setItem("username", JSON.stringify(user));
+    alert("Name: " + nom.value + "\nEmail: " + email.value + "\nPassword: " + password.value);
+});
+
 
 button.addEventListener("click", function(event){
 
-  
-    
     if (nom.value === "") {
     alert("Please fill in username");
     return;
